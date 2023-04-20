44edda313430ebbccc23261ac7d2459582924fef
Kate <68346722+katlazar@users.noreply.github.com>
2023-04-20 19:19:52 +0200

    added cities

--- TEST/src/main/java/org/example/App.java
+++ TEST/src/main/java/org/example/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello hello hello " );
+
+        String[] citiesEU = new String[] {"Berlin", "Sofia", "Wrocław", "Praque"};
+
+        for(String city : citiesEU) {
+            System.out.println(city);
+        }
     }
 }
